ff3be8b9ed33f9c384e7750f612ef2bdc9d935fa
gamancio-tech <gustavoamanciocom@gmail.com>
2026-07-20 20:45:02 -0300

    adicao página de manutenção e consts.ts

---
+++ src/consts.ts
@@ -0,0 +1,7 @@
+// src/consts.ts
+
+export const whatsappHref = (msg: string) =>
+  `https://wa.me/5516988750149?text=${encodeURIComponent(msg)}`;
+export const TELEFONE = "tel:+5516988750149"
+export const INSTAGRAM_HREF = "https://www.instagram.com/infotellfranca_/";
+export const EMAIL_CONTATO = "infotellsuporte@gmail.com";
